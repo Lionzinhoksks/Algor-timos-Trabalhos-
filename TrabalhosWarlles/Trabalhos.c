f27f038029fe46da4b42275b7d6ff68d02285383
Lionzinhoksks <87054493+Lionzinhoksks@users.noreply.github.com>
2025-07-23 11:38:28 -0300

    Update Trabalhos.c

--- TrabalhosWarlles/Trabalhos.c
+++ TrabalhosWarlles/Trabalhos.c
@@ -5,10 +5,6 @@
 int main()
 {
     printf("\t Total \t= 100%\n\t IVA \t= 17%\n\t IRS \t= 15%\n----------------------------- \n\t Liq. \t= 68%\n");
+    
     return 0;
-}
-
-int main2()
-{
-    printf("");
 }
