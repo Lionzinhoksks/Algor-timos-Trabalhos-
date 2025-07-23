4117bb49d031da4a0cf00e78cf06985d18da39be
Lionzinhoksks <87054493+Lionzinhoksks@users.noreply.github.com>
2025-07-23 10:52:58 -0300

    Add files via upload

---
+++ TrabalhosWarlles/TRABalhos.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Dia, Mes, Ano;
+
+
+int main() {
+    printf("Informe um Dia(dd): ", "\n");
+    scanf("%d", &Dia);
+    printf("Informe um Mês(mm): ", "\n");
+    scanf("%d", &Mes);
+    printf("Informe um Ano(aaaa): ", "\n");
+    scanf("%d", &Ano);
+    
+    if(Dia <= 31 && Mes <= 12) {
+        printf("A data informada é: %.2d/%.2d/%d", Dia, Mes, Ano, "\n");
+    } else {
+        printf("Esta data é inválida, insira uma data válida.\n");
+    }
+    
+    
+    return 0;
+}
